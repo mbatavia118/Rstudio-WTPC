49c771b0076bf2371f51edca6a250b068daa8fb8
mbatavia118 <mbatavia@gmail.com>
2020-10-12 23:02:28 -0400

    Followed the instructions on the Coursera course.

---
+++ testing link RStudio to GitHub.R
@@ -0,0 +1,9 @@
+#testing the link between Rstudio and GitHub
+#no packages needed for this project
+
+#clear brain
+rm(list=ls())
+
+#following Coursera's instructions
+print("This file was created within RStudio.")
+print("And now it lives on GitHub!")
